595e8c972a3988e1ce2653ea3c1a6369957666b9
Pronepoet <ochiengphillip84@gmail.com>
2022-09-24 13:46:18 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,15 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+  <div>
+  <nav>NavBar</nav>
+<a href="#home"></a>
+<a href="#about"></a>
+  </div>
+
+  
+  );
 }
 
 export default NavBar;
